9d83b6d21c5ca4eb077c86630e49eba5c68c7f79
faragon <miguelanga1604@gmail.com>
2025-12-09 13:32:52 -0500

    feat: docker

--- Assessment_M6.Application/obj/Debug/net9.0/Assessment_M6.Application.AssemblyInfo.cs
+++ Assessment_M6.Application/obj/Debug/net9.0/Assessment_M6.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assessment_M6.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e9cce4333b9913313c24c3b64112c1643329a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96cfe9f97d409fe0a927d04ff333c55837e192ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assessment_M6.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assessment_M6.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
